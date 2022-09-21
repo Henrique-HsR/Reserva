78991c051869fd242a1e6a87517723286208a5b1
HsRHenrique <hsr.henrique@outlook.com>
2022-09-21 07:40:34 -0300

    modificando buttons

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,3 +1,44 @@
 function validateFields() {
-    const login = document.getElementById('ilogin').value;
+    const loginValid = isLoginValid();
+    document.getElementById("forgot-pass").disabled = !loginValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("btn-login").disabled = !loginValid || !passwordValid;
+    
+
+
+    function isLoginValid () {
+        const login = document.getElementById('ilogin').value;
+
+        if (!login) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function isPasswordValid () {
+        const password = document.getElementById('isenha').value;
+
+        if (!password) {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+function logar() {
+    var login = document.getElementById('ilogin').value;
+    var senha = document.getElementById('isenha').value;
+
+    if (login == "admin" && senha == "admin") {
+        location.href = "./assets/Pages/standard.html";
+    } else {
+        alert('Usuário ou senha incorretos')
+    }
+}
+
+function forgotPass() {
+    location.href = "./assets/Pages/cadastro.html";
 }
